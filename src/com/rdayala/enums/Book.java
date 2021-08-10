ca250308bbbc55c4d2e89409f696e1e02aaf2001
rdayala <rdayala@RDAYALA-IN.oradev.oraclecorp.com>
2021-08-10 19:45:59 +0530

    nested enum concept

---
+++ src/com/rdayala/enums/Book.java
@@ -0,0 +1,35 @@
+package com.rdayala.enums;
+
+// this is an example of nested enum
+
+public class Book {
+
+	// nested enum are implicitly static
+	public enum BookGenere {
+		BIOGRAPHY(12),
+		HORROR(15);
+		
+		private BookGenere(int minAgeToRead) {
+			this.minAgeToRead = minAgeToRead;
+		}
+		
+		// state of enum
+		private int minAgeToRead;
+		public int getMinAgeToRead() {
+			return minAgeToRead;
+		}		
+	}
+	
+	public static void main(String[] args) {
+		for(Book.BookGenere bookGenre : Book.BookGenere.values()) {
+			System.out.println("\nName : " + bookGenre);  // uses toString() method ; toString() internally uses name() method unless overridden.
+			System.out.println(", name() : " + bookGenre.name());
+			System.out.println(", Ordinal : " + bookGenre.ordinal());
+			System.out.println(", Declaring class : " + bookGenre.getDeclaringClass());
+			System.out.println(", compareTo(HORROR) : " + bookGenre.compareTo(Book.BookGenere.HORROR));
+			System.out.println(", equals(HORROR) : " + bookGenre.equals(Book.BookGenere.HORROR));
+			System.out.println(", minAgeToRead : " + bookGenre.getMinAgeToRead()); // getting state of enum object
+		}
+	}
+	
+}
