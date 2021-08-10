10d72e9462a63152758bb5e576cb76a83aeccc77
rdayala <rdayala@RDAYALA-IN.oradev.oraclecorp.com>
2021-08-10 16:43:58 +0530

    Java Hello,World program

---
+++ src/com/rdayala/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.rdayala.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World to Java!!");
+		System.out.println("Java Code Examples!!");
+	}
+
+}
