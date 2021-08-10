0ebc9b1b9cf46c3671420e8a17fcbc5a5f86dc12
rdayala <rdayala@RDAYALA-IN.oradev.oraclecorp.com>
2021-08-10 17:23:29 +0530

    message change

--- src/com/rdayala/hello/HelloWorld.java
+++ src/com/rdayala/hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World to Java!!");
-		System.out.println("Java Code Examples!!");
+		System.out.println("Java Code Examples Repository!!");
 	}
 
 }
